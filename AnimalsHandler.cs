36437593b98dc91e42caf339a77de314e163b2dd
dwalley <danielalley1960@gmail.com>
2021-10-14 13:50:55 -0500

    Add project files.

---
+++ AnimalsHandler.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+
+namespace AnimalsWPF
+{
+    class AnimalsHandler
+    {
+        private static bool _haveDBContext = false;
+
+        public int allCatsIndex = 0;
+        public int allDogsIndex = 0;
+
+        public AnimalsDBEntities ctx = null;
+        public List<Cat> allCatsList = null;
+        public List<Dog> allDogsList = null;
+        public List<Cat> catsList = null;
+        public List<CatBreed> catBreedsList = null;
+        public List<Animal> animalsList = null;
+        public List<Dog> dogsList = null;
+        public List<DogBreed> dogBreedsList = null;
+        public List<Domecile> domecilesList = null;
+        public AnimalsHandler()
+        {
+            //MessageBox.Show("In constructor");
+            if(!_haveDBContext)
+            {
+                try
+                {
+                    ctx = new AnimalsDBEntities();
+                    catsList = ctx.Cats.ToList();
+                    catBreedsList = ctx.CatBreeds.ToList();
+                    animalsList = ctx.Animals.ToList();
+                    dogsList = ctx.Dogs.ToList();
+                    dogBreedsList = ctx.DogBreeds.ToList();
+                    domecilesList = ctx.Domeciles.ToList();
+                    allCatsList = ctx.Cats.Include("CatBreed1").Include("Animal").Include("Domecile").ToList();
+                    allDogsList = ctx.Dogs.Include("DogBreed").Include("Animal").Include("Domecile").ToList();
+                    _haveDBContext = true;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            else
+            {
+                MessageBox.Show("already have DB context");
+            }
+        }
+        public void UpdateCatsIndex()
+        {
+            if (this.allCatsIndex >= this.catsList.Count() - 1)
+            {
+                this.allCatsIndex = 0;
+            }
+            else
+            {
+                this.allCatsIndex += 1;
+            }
+        }
+        public void UpdateDogsIndex()
+        {
+            if (this.allDogsIndex >= this.dogsList.Count() - 1)
+            {
+                this.allDogsIndex = 0;
+            }
+            else
+            {
+                this.allDogsIndex += 1;
+            }
+        }
+        public int GetKeyDomecile(string tempDomecileName)
+        {
+            for (int i=0; i < domecilesList.Count(); ++i)
+                if(domecilesList[i].DomecileName == tempDomecileName)
+                {
+                    return domecilesList[i].DomecileID;
+                }
+            //logic to update Domeciles table with the new name and return the new key
+            Domecile tempRecord = new Domecile();
+            tempRecord.DomecileName = tempDomecileName;
+            ctx.Domeciles.Add(tempRecord);
+            ctx.SaveChanges();
+            this.domecilesList.Add(tempRecord);
+            //MessageBox.Show(tempRecord.DomecileID.ToString());
+            return tempRecord.DomecileID;
+        }
+        public int GetKeyDogBreed(string tempDogBreedName)
+        {
+            for (int i = 0; i < dogBreedsList.Count(); ++i)
+                if (dogBreedsList[i].DogBreedName == tempDogBreedName)
+                {
+                    return dogBreedsList[i].DogBreedID;
+                }
+            //logic to update dog breeds table with the new name and return the new key
+            DogBreed tempRecord = new DogBreed();
+            tempRecord.DogBreedName = tempDogBreedName;
+            ctx.DogBreeds.Add(tempRecord);
+            ctx.SaveChanges();
+            this.dogBreedsList.Add(tempRecord);
+            //MessageBox.Show(tempRecord.DomecileID.ToString());
+            return tempRecord.DogBreedID;
+        }
+        public int GetKeyCatBreed(string tempCatBreedName)
+        {
+            for (int i = 0; i < catBreedsList.Count(); ++i)
+                if (catBreedsList[i].CatBreedName == tempCatBreedName)
+                {
+                    return catBreedsList[i].CatBreedID;
+                }
+            //logic to update catbreeds table with the new name and return the new key
+            CatBreed tempRecord = new CatBreed();
+            tempRecord.CatBreedName = tempCatBreedName;
+            ctx.CatBreeds.Add(tempRecord);
+            ctx.SaveChanges();
+            this.catBreedsList.Add(tempRecord);
+            //MessageBox.Show(tempRecord.DomecileID.ToString());
+            return tempRecord.CatBreedID;
+        }
+        public int GetKeyAnimal(string tempName, string sTempWeight, string sTempBirthday)
+        {
+            for(int i = 0; i < animalsList.Count(); ++i)
+            {
+                if (animalsList[i].Name == tempName)
+                {
+                    MessageBox.Show(tempName + " is already known to us.");
+                    return animalsList[i].AnimalID;
+                }
+            }
+
+            // Convert sTempWeight to a double
+            double tempWeight = Convert.ToDouble(sTempWeight);
+
+            // Convert sTempBirthday to a DateTime
+            DateTime tempBirthday = Convert.ToDateTime(sTempBirthday);
+
+            //MessageBox.Show("Adding Animal" + tempName + tempWeight.ToString() + tempBirthday.ToString());
+
+            //logic to update animals table with the new name and return the new key
+            Animal tempRecord = new Animal();
+            tempRecord.Name = tempName;
+            tempRecord.Weight = tempWeight;
+            tempRecord.Birthday = tempBirthday;
+            ctx.Animals.Add(tempRecord);
+            ctx.SaveChanges();
+            this.animalsList.Add(tempRecord);
+            return tempRecord.AnimalID;
+        }
+        public bool AddCritter(string tempAnimalType, int tempAnimalID, 
+            int tempBreedID, int tempDocecileID, bool tempHobbby)
+        {
+            bool isNewCritter = true;
+
+            switch (tempAnimalType)
+            {
+                case "Dog":
+                    foreach (Dog tempDog in allDogsList)
+                    {
+                        if((tempDog.AnimalID == tempAnimalID) &&
+                            (tempDog.DomecileID == tempDocecileID))
+                        {
+                            // Critter of type dog living at domecile is already in the database, do nothing
+                            isNewCritter = false;
+                            return isNewCritter;
+                        }
+                    }
+                    // This is a new to us dog, add to database.
+                    Dog addMe = new Dog();
+                    addMe.AnimalID = tempAnimalID;
+                    addMe.DogBreedID = tempBreedID;
+                    addMe.DomecileID = tempDocecileID;
+                    addMe.PlaysFetch = tempHobbby;
+                    ctx.Dogs.Add(addMe);
+                    ctx.SaveChanges();
+                    this.dogsList.Add(addMe);
+                    isNewCritter = true;
+                    break;
+                case "Cat":
+                    foreach (Cat tempCat in allCatsList)
+                    {
+                        if ((tempCat.AnimalID == tempAnimalID) &&
+                            (tempCat.DomecileID == tempDocecileID))
+                        {
+                            // Critter of type cat living at domecile is already in the database, do nothing
+                            isNewCritter = false;
+                            return isNewCritter;
+                        }
+                    }
+                    // This is a new to us cat, add to database.
+                    Cat addMe2 = new Cat();
+                    addMe2.AnimalID = tempAnimalID;
+                    addMe2.CatBreed = tempBreedID;
+                    addMe2.DomecileID = tempDocecileID;
+                    addMe2.LikesCatnip = tempHobbby;
+                    ctx.Cats.Add(addMe2);
+                    ctx.SaveChanges();
+                    isNewCritter = true;
+                    this.catsList.Add(addMe2);
+                    break;
+                default:
+                    throw new InvalidOperationException();
+
+            }
+
+            return isNewCritter;
+        }
+        public string GetCatBreed(int tempBreedKey)
+        {
+            for (int i=0; i < catBreedsList.Count(); ++i)
+            {
+                if (tempBreedKey == this.catBreedsList[i].CatBreedID)
+                {
+                    return this.catBreedsList[i].CatBreedName;
+                }
+            }
+            return "";
+        }
+        public Task<string> AsyncGetCatBreed(int tempBreedKey)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetCatBreed(tempBreedKey);
+            });
+        }
+        public string GetDogBreed(int tempBreedKey)
+        {
+            for (int i = 0; i < dogBreedsList.Count(); ++i)
+            {
+                if (tempBreedKey == this.dogBreedsList[i].DogBreedID)
+                {
+                    return this.dogBreedsList[i].DogBreedName;
+                }
+            }
+            return "";
+        }
+        public Task<string> AsyncGetDogBreed(int tempBreedKey)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetDogBreed(tempBreedKey);
+            });
+        }
+        public string GetDomecile(int tempDomecileKey)
+        {
+            for (int i = 0; i < domecilesList.Count(); ++i)
+            {
+                if (tempDomecileKey == this.domecilesList[i].DomecileID)
+                {
+                    return this.domecilesList[i].DomecileName;
+                }
+            }
+            return "";
+        }
+        public Task<string> AsyncGetDomecile(int tempDomecileKey)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetDomecile(tempDomecileKey);
+            });
+        }
+        public Animal GetAnimal(int tempAnimalKey)
+        {
+            for (int i = 0; i < animalsList.Count(); ++i)
+            {
+                if (tempAnimalKey == this.animalsList[i].AnimalID)
+                {
+                    return this.animalsList[i];
+                }
+            }
+            return null;
+        }
+        public Task<Animal> AsyncGetAnimal(int tempAnimalKey)
+        {
+            return Task.Run<Animal>(() =>
+            {
+                randomWait();
+                return this.GetAnimal(tempAnimalKey);
+            });
+        }
+        public void randomWait()
+        {
+            // Wait randomly 2-5 seconds
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            int sleepTime = r.Next(1000, 4000);
+            //MessageBox.Show(sleepTime.ToString());
+            Thread.Sleep(sleepTime);
+        }
+        public string GetAnimalName(Animal tempAnimal)
+        {            
+            return tempAnimal.Name;
+        }
+        public Task<string> AsyncGetAnimalName(Animal tempAnimal)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetAnimalName(tempAnimal);
+            });
+        }
+        public string GetAnimalWeight(Animal tempAnimal)
+        {
+            return tempAnimal.Weight.ToString();
+        }
+        public Task<string> AsyncGetAnimalWeight(Animal tempAnimal)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetAnimalWeight(tempAnimal);
+            });
+        }
+        public string GetAnimalBirthday(Animal tempAnimal)
+        {
+            return tempAnimal.Birthday.ToString();
+        }
+        public Task<string> AsyncGetAnimalBirthday(Animal tempAnimal)
+        {
+            return Task.Run<string>(() =>
+            {
+                randomWait();
+                return this.GetAnimalBirthday(tempAnimal);
+            });
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimalsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AnimalsHandler ahr = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ahr = new AnimalsHandler();
+        }
+
+        private async void btn_ShowCat_Click(object sender, RoutedEventArgs e)
+        {
+            this.ClearForm();
+
+            var tempCat = ahr.catsList[ahr.allCatsIndex];
+
+            int tempAnimalID = Convert.ToInt32(tempCat.AnimalID);
+            Animal tempAnimal = ahr.GetAnimal(tempAnimalID);
+
+            lbl_Hobby.Content = "Likes catnip?";
+            txtBox_Hobby.Text = tempCat.LikesCatnip.ToString();
+
+            int tempCatBreedID = Convert.ToInt32(tempCat.CatBreed);
+            var tempWorker = ahr.AsyncGetCatBreed(tempCatBreedID);
+
+            int tempDomecileID = Convert.ToInt32(tempCat.DomecileID);
+            var tempWorker1 = ahr.AsyncGetDomecile(tempDomecileID);
+
+            var tempWorker2 = ahr.AsyncGetAnimalName(tempAnimal);
+            var tempWorker3 = ahr.AsyncGetAnimalBirthday(tempAnimal);
+            var tempWorker4 = ahr.AsyncGetAnimalWeight(tempAnimal);
+
+            List<Task<string>> tempWorkerTasks = new List<Task<string>>();
+            tempWorkerTasks.Add(tempWorker);
+            tempWorkerTasks.Add(tempWorker1);
+            tempWorkerTasks.Add(tempWorker2);
+            tempWorkerTasks.Add(tempWorker3);
+            tempWorkerTasks.Add(tempWorker4);
+
+            while (tempWorkerTasks.Count > 0)
+            {
+                var tempResult = await Task.WhenAny(tempWorkerTasks);
+                tempWorkerTasks.Remove(tempResult);
+                try
+                {
+                    await tempResult;
+                    if(tempResult == tempWorker)
+                    {
+                        txtBox_Breed.Text = tempResult.Result;
+                    }
+                    else if (tempResult == tempWorker1)
+                    {
+                        txtBox_Home.Text = tempResult.Result;
+                    }
+                    else if (tempResult == tempWorker2)
+                    {
+                        txtBox_Name.Text = tempResult.Result;
+
+                    }
+                    else if (tempResult == tempWorker3)
+                    {
+                        txtBox_Birthday.Text = tempResult.Result;
+                    }
+                    else if (tempResult == tempWorker4)
+                    {
+                        txtBox_Weight.Text = tempResult.Result;
+
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    };
+                }
+                catch(Exception exc)
+                {
+                    throw exc;
+                }
+            }
+
+            ahr.UpdateCatsIndex();
+        }
+
+        private async void btn_ShowDog_Click(object sender, RoutedEventArgs e)
+        {
+            this.ClearForm();
+            var tempDog = ahr.dogsList[ahr.allDogsIndex];
+            lbl_Hobby.Content = "Plays fetch?";
+            txtBox_Hobby.Text = tempDog.PlaysFetch.ToString();
+
+            int tempDogBreedID = Convert.ToInt32(tempDog.DogBreedID);
+            var tempWorker = ahr.AsyncGetDogBreed(tempDogBreedID);
+            await tempWorker;
+            txtBox_Breed.Text = tempWorker.Result;
+
+            int tempDomecileID = Convert.ToInt32(tempDog.DomecileID);
+            var tempWorker1 = ahr.AsyncGetDomecile(tempDomecileID);
+            await tempWorker1;
+            txtBox_Home.Text = tempWorker1.Result;
+
+            int tempAnimalID = Convert.ToInt32(tempDog.AnimalID);
+            var tempWorker2 = ahr.AsyncGetAnimal(tempAnimalID);
+            await tempWorker2;
+            Animal tempAnimal = tempWorker2.Result;
+
+            txtBox_Name.Text = tempAnimal.Name.ToString();
+            txtBox_Weight.Text = tempAnimal.Weight.ToString();
+            txtBox_Birthday.Text = tempAnimal.Birthday.ToString();
+
+            ahr.UpdateDogsIndex();
+        }
+
+        private void btn_AddNewCat_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_Hobby.Content = "Likes catnip?";
+            txtBox_Hobby.Text = "";
+            txtBox_Breed.Text = "";
+            txtBox_Home.Text = "";
+            txtBox_Name.Text = "";
+            txtBox_Weight.Text = "";
+            txtBox_Birthday.Text = "";
+        }
+
+        private void btn_AddNewDog_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_Hobby.Content = "Plays fetch?";
+            txtBox_Hobby.Text = "";
+            txtBox_Breed.Text = "";
+            txtBox_Home.Text = "";
+            txtBox_Name.Text = "";
+            txtBox_Weight.Text = "";
+            txtBox_Birthday.Text = "";
+
+        }
+
+        private void lbl_Update_Click(object sender, RoutedEventArgs e)
+        {
+            string tempAnimalType = "";
+            bool wasAdded = false;
+            switch (lbl_Hobby.Content)
+            {
+                case "Plays fetch?":
+                    tempAnimalType = "Dog";
+                    bool tempHobby = Convert.ToBoolean(txtBox_Hobby.Text);
+                    //MessageBox.Show("Adding a "+tempAnimalType);
+                    if (ConfirmAddition(tempAnimalType))
+                    {
+                        int tempDomecileID = ahr.GetKeyDomecile(txtBox_Home.Text);
+                        int tempDogBreedID = ahr.GetKeyDogBreed(txtBox_Breed.Text);
+                        int tempAnimalID = ahr.GetKeyAnimal(txtBox_Name.Text, txtBox_Weight.Text, txtBox_Birthday.Text);
+                        wasAdded = ahr.AddCritter(tempAnimalType, tempAnimalID, 
+                            tempDogBreedID, tempDomecileID, tempHobby);
+                    }
+                    break;
+                case "Likes catnip?":
+                    tempAnimalType = "Cat";
+                    bool tempHobby2 = Convert.ToBoolean(txtBox_Hobby.Text);
+                    //MessageBox.Show("Adding a "+tempAnimalType);
+                    if (ConfirmAddition(tempAnimalType))
+                    {
+                        int tempDomecileID2 = ahr.GetKeyDomecile(txtBox_Home.Text);
+                        int tempCatBreedID = ahr.GetKeyCatBreed(txtBox_Breed.Text);
+                        int tempAnimalID = ahr.GetKeyAnimal(txtBox_Name.Text, txtBox_Weight.Text, txtBox_Birthday.Text);
+                        wasAdded = ahr.AddCritter(tempAnimalType, tempAnimalID,
+                            tempCatBreedID, tempDomecileID2, tempHobby2);
+                    }
+                    break;
+            }
+            if(wasAdded)
+            {
+                MessageBox.Show("Record added successfully.");
+            }
+
+            ClearForm();
+        }
+        private bool ConfirmAddition(string tempAnimalType)
+        {
+            string tempHobby = txtBox_Hobby.Text;
+            string tempBreed = txtBox_Breed.Text;
+            string tempHome = txtBox_Home.Text;
+            string tempName = txtBox_Name.Text;
+            string tempWeight = txtBox_Weight.Text;
+            string tempBirthday = txtBox_Birthday.Text;
+
+            string tempMessage = tempAnimalType + tempBirthday + tempBreed + tempHobby + tempHome + tempName + tempWeight;
+            var result = MessageBox.Show(tempMessage, "Add to database?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void ClearForm()
+        {
+            lbl_Hobby.Content = "Hobby";
+            txtBox_Hobby.Text = "";
+            txtBox_Breed.Text = "";
+            txtBox_Home.Text = "";
+            txtBox_Name.Text = "";
+            txtBox_Weight.Text = "";
+            txtBox_Birthday.Text = "";
+        }
+    }
+}
